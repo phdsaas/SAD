9f200fca533006e6fa68b33572fc73413545b4f2
jayveev-tm <jayvee_velez@trendmicro.com>
2020-02-01 00:41:53 +0800

    change name

--- Latest/get.sh
+++ Latest/get.sh
@@ -7,7 +7,6 @@ cd temp
 #Download Latest Chart
 curl -LO  https://github.com/deep-security/smartcheck-helm/archive/master.tar.gz
 tar -xvzf master.tar.gz
-cd smartcheck-helm-master
 
 echo "Smart Check version"
-cat Chart.yaml | grep "version"
+cat smartcheck-helm-master/Chart.yaml | grep "version"
