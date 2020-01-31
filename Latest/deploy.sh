5a7c6a9482711cb56a3c5dc669fe8220fb19bea6
jayveev-tm <jayvee_velez@trendmicro.com>
2020-02-01 00:29:53 +0800

    echo version

--- Latest/deploy.sh
+++ Latest/deploy.sh
@@ -6,3 +6,5 @@ cd temp
 #Download Latest Chart
 curl -LO  https://github.com/deep-security/smartcheck-helm/archive/master.tar.gz
 tar -xvzf master.tar.gz
+cd smartcheck-helm-master
+echo Chart.yaml | grep version
