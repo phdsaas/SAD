0ffbb5908e1c2b17638a467f57afee2d9107f97f
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-01 03:13:13 +0800

    fix smartcheck dir

--- Latest/nodeport.sh
+++ Latest/nodeport.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Set service https port to 30443
-sed -ie '/^      targetPort/a \ \ \ \ \ \ nodePort: 30080' smartcheck-helm-master/templates/proxy.yaml >> /dev/null
+sed -ie '/^      targetPort/a \ \ \ \ \ \ nodePort: 30080' ./temp/smartcheck-helm-master/templates/proxy.yaml >> /dev/null
 
 #Set service http port to 30080
-sed '19 s/30080/30443/' smartcheck-helm-master/templates/proxy.yaml >> /dev/null
+sed '19 s/30080/30443/' ./temp/smartcheck-helm-master/templates/proxy.yaml >> /dev/null
 
 #Delete nodePort at line 30
-sed '30d' smartcheck-helm-master/templates/proxy.yaml >> /dev/null
+sed '30d' ./temp/smartcheck-helm-master/templates/proxy.yaml >> /dev/null
 
-cat smartcheck-helm-master/templates/proxy.yaml
+cat ./temp/smartcheck-helm-master/templates/proxy.yaml
