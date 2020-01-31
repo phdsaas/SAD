0a28a46e0418d712bd4d21742d64bd68701dd34b
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-01 02:37:14 +0800

    Update get.sh

--- Latest/get.sh
+++ Latest/get.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -rf *
+rm -rf temp/
+mkdir temp/
+cd temp/
 
 #Download Latest Chart
 curl -LO  https://github.com/deep-security/smartcheck-helm/archive/master.tar.gz
@@ -8,3 +10,5 @@ tar -xvzf master.tar.gz
 
 echo "Smart Check version"
 cat smartcheck-helm-master/Chart.yaml | grep "version"
+
+cd ..
