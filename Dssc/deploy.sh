fa718708a5765c917e9b6b5d5ecee3a76d43f6ad
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-04 00:54:34 +0800

    Deploy DSSC

--- Dssc/deploy.sh
+++ Dssc/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-kubectl version
-kubectl get nodes
+helm install --set persistence.enabled=false --set activationCode=$ACTIVATION_CODE --set auth.secretSeed=$DSSC_SECRET_SEED --set service.type=NodePort deepsecurity-smartcheck ./temp/smartcheck-helm-master/
