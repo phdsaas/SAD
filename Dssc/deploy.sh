2fbb317ad6925820d4bb39f89dbbf52deb8549fd
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-04 01:12:43 +0800

    Update deploy.sh

--- Dssc/deploy.sh
+++ Dssc/deploy.sh
@@ -3,4 +3,4 @@
 kubectl delete networkpolicies proxy
 sleep 2s
 
-helm install --set service.type=NodePort  --set persistence.enabled=false --set activationCode=$ACTIVATION_CODE --set auth.secretSeed=$DSSC_SECRET_SEED deepsecurity-smartcheck ./temp/smartcheck-helm-master/.
+helm install --debug --set service.type=NodePort  --set persistence.enabled=false --set activationCode=$ACTIVATION_CODE --set auth.secretSeed=$DSSC_SECRET_SEED deepsecurity-smartcheck ./temp/smartcheck-helm-master/.
