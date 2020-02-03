2ddc682bc8c664083761f0638a3bccd2c68b856d
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-04 02:21:10 +0800

    Test direct download

--- Dssc/deploy.sh
+++ Dssc/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 helm delete --debug deepsecurity-smartcheck
 sleep 5s
-helm install --debug --set service.type=NodePort  --set persistence.enabled=false --set activationCode="$ACTIVATION_CODE" --set auth.secretSeed="$DSSC_SECRET_SEED" deepsecurity-smartcheck ./temp/smartcheck-helm-master/.
+#helm install --debug --set service.type=NodePort  --set persistence.enabled=false --set activationCode="$ACTIVATION_CODE" --set auth.secretSeed="$DSSC_SECRET_SEED" deepsecurity-smartcheck ./temp/smartcheck-helm-master/.
+helm install --debug --set service.type=NodePort  --set persistence.enabled=false --set activationCode="$ACTIVATION_CODE" --set auth.secretSeed="$DSSC_SECRET_SEED" deepsecurity-smartcheck https://github.com/deep-security/smartcheck-helm/archive/master.tar.gz
