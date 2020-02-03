3100c9df7ce2ea4b0b021c1045e403d329c61daf
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-04 01:32:20 +0800

    Update deploy.sh

--- Dssc/deploy.sh
+++ Dssc/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+helm delete --debug deepsecurity-smartcheck
+sleep 5s
 helm install --debug --replace --set service.type=NodePort  --set persistence.enabled=false --set activationCode="$ACTIVATION_CODE" --set auth.secretSeed="$DSSC_SECRET_SEED" deepsecurity-smartcheck ./temp/smartcheck-helm-master/.
