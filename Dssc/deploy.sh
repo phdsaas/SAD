9fc4a680a03a13b63b45726c56581962a92fc5ee
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-04 04:21:18 +0800

    remove sudo

--- Dssc/deploy.sh
+++ Dssc/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo helm install --debug --wait --timeout 10m --set service.type=NodePort  --set persistence.enabled=false --set activationCode=$ACTIVATION_CODE --set auth.secretSeed=$DSSC_SECRET_SEED deepsecurity-smartcheck https://github.com/deep-security/smartcheck-helm/archive/master.tar.gz
+helm install --debug --wait --timeout 10m --set service.type=NodePort  --set persistence.enabled=false --set activationCode=$ACTIVATION_CODE --set auth.secretSeed=$DSSC_SECRET_SEED deepsecurity-smartcheck https://github.com/deep-security/smartcheck-helm/archive/master.tar.gz
