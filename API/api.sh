a70793a5bc14ee579ce6b87a8c63133bd0364bdd
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-06 02:11:09 +0800

    add api call for add registry

--- API/api.sh
+++ API/api.sh
@@ -1,8 +1,24 @@
-export API_TOKEN=$(curl -k https://smartcheck.jayveev.tmi:30443/api/sessions --header "Content-Type: application/json" --request POST --data '{
+export DSSC_TOKEN=$(curl -k https://smartcheck.jayveev.tmi:30443/api/sessions --header "Content-Type: application/json" --request POST --data '{
 "user": {
 "userID": "'"$DSSC_USER"'",
 "password": "'"$DSSC_PASS"'"
 }
 }' | jq -r '.token')
 
-echo $API_TOKEN
+curl -k https://smartcheck.jayveev.tmi:30443/api/registries --header "Content-Type: application/json" --header "Authorization: Bearer $DSSC_TOKEN" --request POST --data '{
+"name": "PHDSaaS ECR",
+"description": "This registry is manages by PH DSaaS Support Team",
+"host": "smartcheck.jayveev.tmi",
+"credentials": {
+"username": "'"$DSSC_USER"'",
+"password": "'"$DSSC_PASS"'",
+"aws": {
+"region": "'"$AWS_REGION"'",
+"accessKeyID": "'"$AWS_AK"'",
+"secretAccessKey": "'"$AWS_SK"'",
+"registry": "'"$AWS_ACCOUNT_ID"'"
+}
+},
+"insecureSkipVerify": true,
+"schedule": false
+}'
