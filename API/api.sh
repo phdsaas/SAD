188a80e22cc92517a150a3f96871f24060fcfe0f
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-06 01:57:19 +0800

    export token to variable

--- API/api.sh
+++ API/api.sh
@@ -1,8 +1,8 @@
-curl -k https://smartcheck.jayveev.tmi:30443/api/sessions --header "Content-Type: application/json" --request POST --data '{
+export API_TOKEN=$(curl -k https://smartcheck.jayveev.tmi:30443/api/sessions --header "Content-Type: application/json" --request POST --data '{
 "user": {
 "userID": "'"$DSSC_USER"'",
 "password": "'"$DSSC_PASS"'"
 }
-}' >> token.txt
+}' | jq -r '.token')
 
-cat token.txt
+echo $API_TOKEN
