8cc6653295e22a5c6f23f8fa0bd7007d9d53a4a6
Jayvee Velez <52455111+jayveev-tm@users.noreply.github.com>
2020-02-06 01:15:13 +0800

    Update api.sh

--- API/api.sh
+++ API/api.sh
@@ -1,6 +1,6 @@
 curl -k https://smartcheck.jayveev.tmi:30443/api/sessions --header "Content-Type: application/json" --request POST --data '{
 "user": {
 "userID": "administrator",
-"password": "f9@boiS74!DIZwcF0EUE"
+"password": "$DSSC_PASS"
 }
 }'
